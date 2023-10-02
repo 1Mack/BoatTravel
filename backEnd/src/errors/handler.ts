8b9212dd6494e1d08792096916ba78afe426e4da
1MacK <maaaaaack@outlook.com>
2023-10-02 11:43:11 -0300

    initialize dotenv

--- backEnd/src/errors/handler.ts
+++ backEnd/src/errors/handler.ts
@@ -10,7 +10,7 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
     let errors: ValidationErrors = {};
 
     error.inner.forEach((err) => {
-      errors[err.path] = err.errors;
+      if (err && err.path) errors[err.path] = err.errors;
     });
 
     return response.json({ message: 'Validation failed', error: errors }).status(404)
@@ -18,7 +18,7 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
 
   if (error.message.includes('Unique constraint'))
     return response.json({ message: `There is already a data registered with the value of the field ${error.meta.target}`, error }).status(400)
-    
+
   return response.json({ message: 'Internal server error', error }).status(500)
 }
 
--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import 'express-async-errors';
-
+import 'dotenv/config'
 import routes from './routes'
 import errorHandler from './errors/handler';
 
